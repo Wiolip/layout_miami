f25c266fcd04a6e2714fdb04370dbb13bc4ec7b1
Wioleta Lip <wiolip@hotmail.com>
2025-10-07 21:57:32 +0200

    changes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const contactForm = document.querySelector('.form');
+
+// @ts-ignore
+contactForm.addEventListener('submit', function (event) {
+
+  event.preventDefault();
+  this.reset();
+});
