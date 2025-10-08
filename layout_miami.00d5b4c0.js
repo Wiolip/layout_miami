dd3ce56a8c1807cc9969ddfa6b72837e9d20d943
Wioleta Lip <wiolip@hotmail.com>
2025-10-08 22:41:36 +0200

    make build

---
+++ layout_miami.00d5b4c0.js
@@ -0,0 +1,2 @@
+"use strict";var contactForm=document.querySelector(".form");contactForm.addEventListener("submit",function(t){t.preventDefault(),this.reset()});
+//# sourceMappingURL=layout_miami.00d5b4c0.js.map
